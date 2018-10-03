6276757a4b58c14c5bf42f8460cb4a4d8888b451
Justin T. Dejesus <rr605855@fincoad.com>
2018-10-03 10:09:52 -0500

    add redux devtools

--- src/state/store/store.js
+++ src/state/store/store.js
@@ -1,4 +1,8 @@
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import rootReducer from '../reducers/rootReducer';
 
-export default createStore(rootReducer);
+export default createStore(
+    rootReducer,
+    devToolsEnhancer()
+    );
