52c6b4fd6acdad473ef895d6315db449f3af0102
Justin T. Dejesus <rr605855@fincoad.com>
2018-10-04 08:52:12 -0500

    modified unit test to ensure provider/store will render

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './state/store/store';
 import App from './App';
 
-it('renders without crashing', () => {
+
+it('renders with store provider without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  , div);
   ReactDOM.unmountComponentAtNode(div);
 });
